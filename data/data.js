8f25ca126bc42f68c5dfa448cf4f0eb4f59e5800
CodeLizards <ebharris93@gmail.com>
2016-03-27 21:33:11 -0700

    Add lib files and data

---
+++ data/data.js
@@ -0,0 +1,19 @@
+var imageData = [
+  {
+    title: 'Puppy',
+    image: 'http://www.pawderosa.com/images/puppies.jpg',
+    rating: 0
+  },
+  {
+    title: 'Panda',
+    image: 'https://s-media-cache-ak0.pinimg.com/originals/1b/f7/3a/1bf73a1a2595125c17c156631140a028.jpg',
+    rating: 0
+  },
+  {
+    title: 'Panther',
+    image: 'https://41.media.tumblr.com/c38e8fdc23f590ccfc01c8ee35c608f6/tumblr_nlwg2hBqL21qi4ucgo1_500.jpg',
+    rating: 0
+  }
+];
+
+window.imageData = imageData;
