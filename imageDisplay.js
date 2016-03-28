8db53adc44a026c3d366470e19b47ffc76a8a10e
CodeLizards <ebharris93@gmail.com>
2016-03-27 21:32:26 -0700

    Implement component display component to display the clicked image

---
+++ imageDisplay.js
@@ -0,0 +1,37 @@
+class ImageDisplay extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  handleChange() {
+    console.log('changed!');
+    this.props.updateRating(this.props.image.image, this.refs.rating.value)
+    
+    // this.props.image.rating = this.refs.rating.value;
+    // this.render();
+  }
+
+  render() {
+    return (
+      <div className='col-2'>
+        <img src={this.props.image.image} />
+        <span>{this.props.image.name}</span>
+        <select ref="rating" onChange={this.handleChange.bind(this)}>
+          
+          <option>0</option>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+
+        </select>
+        <span className="image-description">{' ' +this.props.image.rating+' out of 5'}</span>
+      </div>
+
+
+    )
+  }
+}
+
+window.ImageDisplay = ImageDisplay; 
