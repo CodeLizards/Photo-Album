81d9e1fcda8dab51eb5126a2da5aaec4bfe210cf
CodeLizards <ebharris93@gmail.com>
2016-03-27 21:33:49 -0700

    Implement form that takes input from user and add the photo to the album

---
+++ newImage.js
@@ -0,0 +1,49 @@
+class NewImage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  handleSubmit() {
+    // console.log('values', this.refs.imageUrl.value);
+    // console.log('values', this.refs.imageTitle.value);
+    this.props.addImage(this.refs.imageUrl.value, this.refs.imageTitle.value);
+    return false;
+
+  }
+
+  validateForm() {
+        return false;
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+        
+        <div>
+          <label>
+          Image Url:
+          </label>
+          <input ref='imageUrl'>
+          </input>
+        </div>
+        
+        <div>
+          <label>
+            Image Title:
+          </label>
+          <input ref='imageTitle'>
+          </input>
+        </div>
+
+        </form>
+
+        <button onClick={this.handleSubmit.bind(this)}>Post Image</button>
+
+      </div>
+
+    )
+  }
+};
+
+window.NewImage = NewImage;
